ed819e343a4e0d056534ae33243be11060f57424
Lee Sutton <leesutton1@gmail.com>
2016-09-02 10:53:24 -0700

    Added arguement parser and boundaries for skin detection

---
+++ Skin-detection/skindetection.py
@@ -0,0 +1,14 @@
+import imutils
+import numpy as np
+import argparse
+import cv2
+
+# Construct the arguement parser
+ap = argparse.ArgumentParser()
+ap.add_argument("-v","--video", help = "path to the optional video file")
+args = vars(ap.parse_args())
+
+# define the upper and lower boundaries of the HSV pixel
+# intensities to be considered 'skin'
+lower = np.array([0, 48, 80], dtype = "uint8")
+upper = np.array([20, 255, 255], dtype = "uint8")
