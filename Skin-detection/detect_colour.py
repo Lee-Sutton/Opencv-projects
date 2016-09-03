576219bd89842e161cf2e8b585c948a183740aca
Lee Sutton <leesutton1@gmail.com>
2016-09-02 20:07:55 -0700

    detect colour finished. Skin detection started

--- Skin-detection/detect_colour.py
+++ Skin-detection/detect_colour.py
@@ -5,8 +5,31 @@ import cv2
 
 # Construct the ArgumentParser
 ap = argparse.ArgumentParser()
-ap.add_argument("-i","--image", help = "path to the image")
-args = vars(ap.parse_args)
+ap.add_argument("-i", "--image", help = "path to the image")
+args = vars(ap.parse_args())
 
 # load the image
 image = cv2.imread(args["image"])
+
+
+# define boundaries for the colours to be detected
+boundaries = [
+	([17, 15, 100], [50, 56, 200]),
+	([86, 31, 4], [220, 88, 50]),
+	([25, 146, 190], [62, 174, 250]),
+	([103, 86, 65], [145, 133, 128])
+]
+
+# Now we can use the in range function to perform the colour detection
+for (lower, upper) in boundaries:
+    lower = np.array(lower, dtype = "uint8")
+    upper = np.array(upper, dtype = "uint8")
+
+    # find the colors within the specified boundaries and apply
+	# the mask
+    mask = cv2.inRange(image, lower, upper)
+    output = cv2.bitwise_and(image, image, mask = mask)
+
+    # show the images
+    cv2.imshow("images", np.hstack([image, output]))
+    cv2.waitKey(0)
--- Skin-detection/skindetection.py
+++ Skin-detection/skindetection.py
@@ -12,3 +12,20 @@ args = vars(ap.parse_args())
 # intensities to be considered 'skin'
 lower = np.array([0, 48, 80], dtype = "uint8")
 upper = np.array([20, 255, 255], dtype = "uint8")
+
+# If a video path was not supplied, the web cam will be used
+if not args.get("video", False):
+    camera = cv2.VideoCapture(0)
+
+# Otherwise, load the given video file
+else:
+    camera = cv2.VideoCapture(args["video"])
+
+# Keep looping over the frames in the video
+while True:
+    # grab the current frame
+    (grabbed, frame) = camera.read()
+
+    # if we are viewing a video and we did not grab a frame, we have reached the end
+    if args.get("video") and not grabbed:
+        break
