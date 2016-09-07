d2b1dd8fccdbab273ddf49b922343f050ecddce1
Lee Sutton <leesutton1@gmail.com>
2016-09-07 13:43:14 -0700

    Tested skin detection with webcam

--- Skin-detection/skindetection.py
+++ Skin-detection/skindetection.py
@@ -21,11 +21,41 @@ if not args.get("video", False):
 else:
     camera = cv2.VideoCapture(args["video"])
 
-# Keep looping over the frames in the video
+# keep looping over the frames in the video
 while True:
-    # grab the current frame
-    (grabbed, frame) = camera.read()
+	# grab the current frame
+	(grabbed, frame) = camera.read()
 
-    # if we are viewing a video and we did not grab a frame, we have reached the end
-    if args.get("video") and not grabbed:
-        break
+	# if we are viewing a video and we did not grab a
+	# frame, then we have reached the end of the video
+	if args.get("video") and not grabbed:
+		break
+
+	# resize the frame, convert it to the HSV color space,
+	# and determine the HSV pixel intensities that fall into
+	# the speicifed upper and lower boundaries
+	frame = imutils.resize(frame, width = 400)
+	converted = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	skinMask = cv2.inRange(converted, lower, upper)
+
+	# apply a series of erosions and dilations to the mask
+	# using an elliptical kernel
+	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+	skinMask = cv2.erode(skinMask, kernel, iterations = 2)
+	skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
+
+	# blur the mask to help remove noise, then apply the
+	# mask to the frame
+	skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
+	skin = cv2.bitwise_and(frame, frame, mask = skinMask)
+
+	# show the skin in the image along with the mask
+	cv2.imshow("images", np.hstack([frame, skin]))
+
+	# if the 'q' key is pressed, stop the loop
+	if cv2.waitKey(1) & 0xFF == ord("q"):
+		break
+
+# cleanup the camera and close any open windows
+camera.release()
+cv2.destroyAllWindows()
