d5b1e22653724981a1227915b41e7dffc3674a75
Lee Sutton <leesutton1@gmail.com>
2016-09-07 15:11:56 -0700

    Created color transfer file and imported libraries

---
+++ Colour-transfer/colour-transfer.py
@@ -0,0 +1,10 @@
+# Import necessary libraries
+import numpy as np
+import cv2
+
+def color_transfer(source, target):
+	# convert the images from the RGB to L*ab* color space, being
+	# sure to utilizing the floating point data type (note: OpenCV
+	# expects floats to be 32-bit, so use that instead of 64-bit)
+	source = cv2.cvtColor(source, cv2.COLOR_BGR2LAB).astype("float32")
+	target = cv2.cvtColor(target, cv2.COLOR_BGR2LAB).astype("float32")
